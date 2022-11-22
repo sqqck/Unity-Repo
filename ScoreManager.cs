bbd73fea86e1797058306c5ca60c52d1e04af6c0
sqqck <koraygench@gmail.com>
2022-11-23 02:19:29 +0300

    updating Score Manager

--- ScoreManager.cs
+++ ScoreManager.cs
@@ -1,1 +1,27 @@
-a
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] private Text Score;
+    public int _score = 0;
+    void Start()
+    {
+        UpdateScore();
+    }
+    public void AddScore(){
+        _score += 1;
+        UpdateScore();
+    }
+    public void RemoveScore(){
+        _score -= 1;
+        UpdateScore();
+    }
+
+    public void UpdateScore(){
+        Score.text = "Score : " + _score.ToString();
+    }
+    public int GetScore(){
+        return _score;
+    }
+}
