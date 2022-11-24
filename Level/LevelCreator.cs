82e771ff6b4a2c283b26bb34e04d793b099391bd
sqqck <koraygench@gmail.com>
2022-11-24 14:02:49 +0300

    fix: Object creation bugs

--- Level/LevelCreator.cs
+++ Level/LevelCreator.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class LevelCreator : MonoBehaviour
 {
-
-    //todo: add collectable block much than barrier blocks
-    //todo: add new chance system
-    public Transform player;
-
     public List<GameObject> createdObjects = new List<GameObject>();
     public GameObject floorPrefab;
     public GameObject collectableCubePrefab;
@@ -22,41 +17,43 @@ public class LevelCreator : MonoBehaviour
     {
         FloorCreation();
         FinishLineCreation();
+        Collectables();
         BlockCreation();
     }
 
-    
     private void FloorCreation(){
-        floorLength = Random.Range(150f, 200.0f);
-        floorWidth = 10f;
+        floorLength = Random.Range(150.0f, 200.0f);
         var floorPosition = new Vector3(0, 0, floorLength*.5f-10);
         GameObject floor = Instantiate (floorPrefab, floorPosition, Quaternion.identity);
-        SetParentGenerator(floor);
-        floor.transform.localScale = new Vector3(floorWidth, 1, floorLength);
+        SetParent(floor);
+        floor.transform.localScale = new Vector3(10f, 1, floorLength);
     }
 
     private void BlockCreation(){
         if (createdObjects[createdObjects.Count-1].transform.position.z > finishLinePosition.z-20f){
             return;
         }
-        if(Random.value<0.75f){
+        if(Random.value<0.25f){
             Collectables();
         }else{
             Barriers();
         }
     }
+
     private void Collectables(){
         GameObject objects = Instantiate (collectableCubePrefab, new Vector3(Random.Range(-4.5f, 4.5f),1,Random.Range(5f, 10f)+createdObjects[createdObjects.Count-1].transform.position.z+5f), Quaternion.identity);
-        SetParentGenerator(objects);
+        SetParent(objects);
         createdObjects.Add(objects);
         BlockCreation();
     }
+
     private void Barriers(){
         if (createdObjects[createdObjects.Count-1].tag == "Barrier"){
+            BlockCreation();
             return;
         }
         GameObject objects = Instantiate (barrierPrefab, new Vector3(0,1,Random.Range(5f, 10f)+createdObjects[createdObjects.Count-1].transform.position.z+5f), Quaternion.identity);
-        SetParentGenerator(objects);
+        SetParent(objects);
         createdObjects.Add(objects);
         BlockCreation();
     }
@@ -64,11 +61,11 @@ public class LevelCreator : MonoBehaviour
     private void FinishLineCreation(){
         finishLinePosition = new Vector3(0, 0, (floorLength)*.75f);
         GameObject finishLine = Instantiate (finishLinePrefab, finishLinePosition, Quaternion.identity);
-        SetParentGenerator(finishLine);
-        finishLine.transform.localScale = new Vector3(floorWidth-.5f, 1.1f, 1);
+        SetParent(finishLine);
+        finishLine.transform.localScale = new Vector3(9.5f, 1.1f, 1);
     }
 
-    private void SetParentGenerator(GameObject _go){
+    private void SetParent(GameObject _go){
         _go.transform.SetParent(transform);
     }
 }
